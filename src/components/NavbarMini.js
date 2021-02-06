1c23ba91ed6ae6503073b5c385e41fe2650c1c82
Gregory Papas <csd3824@csd.uoc.gr>
2021-02-06 14:15:49 -0800

    home page completed with autoplay slideshow

---
+++ src/components/NavbarMini.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./NavbarMini.css"
+
+function NavbarMini() {
+    return (
+        <div className="NavbarMini">
+            <div className="leftSide"><p>Τηλέφωνα Επικοινωνίας</p></div>
+           <div className="rightSide"><p>Γλώσσα</p></div>
+        </div>
+    )
+}
+
+export default NavbarMini
